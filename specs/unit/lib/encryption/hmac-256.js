9914d6ac0a47699af2e6078023e171d020b8961c
Lee Crowe <lee.crowe@comparethemarket.com>
2015-11-09 13:26:52 +0000

    Adding new tests for hmac-256 encryption

---
+++ specs/unit/lib/encryption/hmac-256.js
@@ -0,0 +1,13 @@
+require('should');
+var encryptor = require('../../../../lib/encryption/hmac-256');
+
+describe('HMAC-256 lib', function() {
+
+    it('correctly hashes a string', function() {
+
+        var hash = encryptor.encrypt('TheValueToEncrypt', { password: 'abcdefghi=' });
+
+        hash.should.eql('JJ0E6ey9ggRWs5+dokPxI1HXIwMQTVxF+DSXL43VCc8=');
+
+    });
+})
